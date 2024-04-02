4d38cb18687fdc64499670a77a367e588caafffa
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2024-04-02 12:46:09 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -3,7 +3,8 @@ class script(object):
 Mʏ Nᴀᴍᴇ Is <a href=https://t.me/{}>{}</a>, I Cᴀɴ Pʀᴏᴠɪᴅᴇ Mᴏᴠɪᴇs, Jᴜsᴛ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ Aɴᴅ Eɴᴊᴏʏ, Oʀ SΞᴀʀᴄʜ Hᴇʀᴇ 😋😋</b>
 <blockquote>Bᴏᴛ Suᴩᴩᴏʀᴛ Oɴʟʏ ɪɴ ɢʀᴏUᴩ🥲</blockquote>
 <blockquote>Fᴏʀ <b>PM</b> Sᴜᴩᴩ0ят ᴜꜱᴇ @MrMoVieZ_bOt 🎈</blockquote>
-<blockquote>Δᴅᴅ ᴍᴇ Tᴏ ʏᴏᴜʀ ɢʀᴏᴜᴩ ᴀɴᴅ ᴇᴀʀɴ ᴍᴏɴᴇʏ🤭</blockquote>"""
+<blockquote>Δᴅᴅ ᴍᴇ Tᴏ ʏᴏᴜʀ ɢʀᴏᴜᴩ ᴀɴᴅ ᴇᴀʀɴ ᴍᴏɴᴇʏ🤭</blockquote>
+"""
 
     HELP_TXT = """<b>Hᴇʏ {}
 Hᴇʀᴇ Is Tʜᴇ Hᴇʟᴘ Fᴏʀ Mʏ Cᴏᴍᴍᴀηᴅs.</b>"""
