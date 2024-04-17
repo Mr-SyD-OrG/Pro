e49624d55a02dd760b5eddf9a991ac487799d29e
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2024-04-17 17:52:33 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1239,7 +1239,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         )
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
-            text=script.FILETOLINK_TXT,
+            text=script.FILETOLINK,
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
