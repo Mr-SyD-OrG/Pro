657ce1db0757b53301d44699a8d9af994e371c6a
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2024-11-03 08:01:36 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -20,6 +20,9 @@ BATCH_FILES = {}
 
 @Client.on_message(filters.command("start") & filters.incoming)
 async def start(client, message):
+    SU = "😀 ⚡ 👍 ❤️ 🔥 👀 👏 🕊️ 😘 🆒 🍓 🏆 💯 ❤️‍🔥"
+    SYDD = PI.split()
+    await message.react(emoji=random.choice(SYDD))
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
         buttons = [[
                     InlineKeyboardButton('⤬ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ ⤬', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
