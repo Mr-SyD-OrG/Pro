830e95542e0c39859a3d9f42d982cd02ccaade7e
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2024-11-16 07:59:25 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -92,7 +92,7 @@ async def leave_a_chat(bot, message):
             reply_markup=reply_markup,
         )
 
-       await bot.leave_chat(chat)
+        await bot.leave_chat(chat)
         await message.reply(f"left the chat `{chat}`")
     except Exception as e:
         await message.reply(f'Error - {e}')
